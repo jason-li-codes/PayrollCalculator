7dd4df52ddf43a47a8f4755ff55ad2dbcbf228b5
jasonli <lijason991@gmail.com>
2025-10-07 13:58:05 -0400

    first commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,51 @@
+public class Employee {
+
+    private String employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    public Employee(String employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getGrossPay() {
+        return this.hoursWorked * this.payRate;
+    }
+
+}
